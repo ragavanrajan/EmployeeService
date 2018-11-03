734889fa7758130a04013c4d5e8bbd84048337bf
Ragavan Rajan <rr100@students.waikato.ac.nz>
2018-11-03 21:40:04 +1300

    EmployeeService API

--- EmployeeService/Controllers/ValuesController.cs
+++ EmployeeService/Controllers/ValuesController.cs
@@ -5,35 +5,43 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace EmployeeService.Controllers
+namespace WebAPIDemo.Controllers
 {
     public class ValuesController : ApiController
     {
+        private static List<string> strings = new List<string>()
+        {
+            "value0", "value1", "value2"
+        };
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return strings;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return strings[id];
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            strings.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            strings[id] = value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            strings.RemoveAt(id);
         }
     }
 }
+
